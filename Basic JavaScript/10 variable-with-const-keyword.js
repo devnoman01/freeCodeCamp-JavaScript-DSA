b7fcfc8488cc35123b8cb98cab4da1d0008dbee9
devnoman01 <devnoman01@gmail.com>
2022-01-28 03:58:35 +0600

    variable declare with const keyword

---
+++ Basic JavaScript/10 variable-with-const-keyword.js
@@ -0,0 +1,9 @@
+//  variable declare with const keyword
+
+
+//  once a variable is assigned with const, it cannot be reassigned
+//  developers use uppercase variable which used with const
+const FAV_PET = "Cats";
+
+FAV_PET = "Dogs";
+//  results error due to reassigning the value of FAV_PET
