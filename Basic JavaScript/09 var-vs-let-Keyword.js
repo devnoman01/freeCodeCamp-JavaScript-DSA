d39cbb39500afd1b5967efd88d6bf7140743f03b
devnoman01 <devnoman01@gmail.com>
2022-01-28 03:38:15 +0600

    var vs let keyword differences

---
+++ Basic JavaScript/09 var-vs-let-Keyword.js
@@ -0,0 +1,11 @@
+//  var vs let keyword differences
+
+//  variables with var keyword can be easily overwritten
+var camper = "James";
+var camper = "David";
+
+
+//  when declared with let keyword, a variable with same name can only be declared once
+let camper = "James";
+let camper = "David";
+//  error will occur if we try to use a var twice
