618ccdcf9a3980f1585a7aa996a2eee8e3c43b5d
devnoman01 <devnoman01@gmail.com>
2022-01-28 02:46:25 +0600

    variable to variable value assignment

---
+++ Basic JavaScript/var-to-var-value-assignment.js
@@ -0,0 +1,8 @@
+// variable to variable value assignment
+
+
+var a;      // declare variable a
+a = 7;      // initialize variable a with value
+
+var b;      // declare variable b
+b = a;      // assigning value of var a into var b
