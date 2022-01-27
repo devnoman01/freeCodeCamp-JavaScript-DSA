66079be26d7a37c56196b3e0a47aee2e6c220d01
devnoman01 <devnoman01@gmail.com>
2022-01-28 03:28:48 +0600

    variable case sensitivity

---
+++ Basic JavaScript/08 variable-case-sensitivity.js
@@ -0,0 +1,26 @@
+//  variable case sensitivity
+
+var MYVAR;
+var MyVar;
+var myvar;
+//  here MYVAR, MyVar and myvar are different variable
+
+
+//  without formating
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+
+//  with formating
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
