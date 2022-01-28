a211618f3770bc125265347361e538dc9abeb873
devnoman01 <devnoman01@gmail.com>
2022-01-29 02:32:27 +0600

    augmented addition

---
+++ Basic JavaScript/21 augmented-addition.js
@@ -0,0 +1,11 @@
+//  augmented addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;        // here 12 will be added with the value of a
+b += 9;         // here 9 will be added with the value of b
+c += 7;         // here 7 will be added with the value of c
+
+//  '+='  assignment operator is used for augmented addition
