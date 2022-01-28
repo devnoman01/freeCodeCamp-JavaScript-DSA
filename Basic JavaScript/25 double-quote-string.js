6ac10c863fc4cd93b871a8f942358fccb31364c4
devnoman01 <devnoman01@gmail.com>
2022-01-29 03:35:45 +0600

    string with double quotes

---
+++ Basic JavaScript/25 double-quote-string.js
@@ -0,0 +1,7 @@
+//  string with double quotes
+
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//  for double quote in string \" used at start and end
