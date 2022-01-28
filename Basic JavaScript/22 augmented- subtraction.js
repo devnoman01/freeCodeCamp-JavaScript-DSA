878c6724139dacd09604aa3a4827282616f31c5f
devnoman01 <devnoman01@gmail.com>
2022-01-29 03:21:16 +0600

    augmented subtraction

--- Basic JavaScript/22 augmented- subtraction.js
+++ Basic JavaScript/22 augmented- subtraction.js
@@ -1,11 +1,11 @@
-//  augmented multiplication
+//  augmented subtraction
 
-let a = 5;
-let b = 12;
-let c = 4.6;
+let a = 11;
+let b = 9;
+let c = 3;
 
-a *= 5;         // here value of a will be multiplied by 5 
-b *= 3;         // here value of b will be multiplied by 3
-c *= 10;        // here value of c will be multiplied by 10
+a -= 6;         // here 6 will be subtracted from the value of a
+b -= 15;        // here 15 will be subtracted from the value of b
+c -= 1;         // here 1 will be subtracted from the value of c
 
-//  '*='  assignment operator is used for augmented multiplication
+//  '-='  assignment operator is used for augmented subtraction
