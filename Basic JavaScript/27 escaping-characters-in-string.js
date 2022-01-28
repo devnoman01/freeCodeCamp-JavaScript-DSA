cbc611d6a9bac3b877e9af460cc29c66988466cc
devnoman01 <devnoman01@gmail.com>
2022-01-29 04:01:00 +0600

    escaping character use in string

---
+++ Basic JavaScript/27 escaping-characters-in-string.js
@@ -0,0 +1,19 @@
+//  escaping character use in string
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+
+/*
+
+Code	Output
+\'	    single quote
+\"	    double quote
+\\	    backslash
+\n	    newline
+\r	    carriage return
+\t	    tab
+\b	    word boundary
+\f	    form feed
+
+*/
+
