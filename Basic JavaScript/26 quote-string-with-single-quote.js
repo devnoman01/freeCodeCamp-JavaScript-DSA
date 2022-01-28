af5bcfbba122a5ff6935269fba92d4720f338568
devnoman01 <devnoman01@gmail.com>
2022-01-29 03:45:41 +0600

    quoting string with single quotes

---
+++ Basic JavaScript/26 quote-string-with-single-quote.js
@@ -0,0 +1,19 @@
+//  quoting string with single quotes
+
+const doubleQuoteStr = "This is a string"; 
+const singleQuoteStr = 'This is also a string';
+
+const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+
+const goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+
+const sampleStr = 'Alan said, "Let\'s learn JavaScript".';
+
+const myStr = '<a href=\"http://www.example.com\" target=\"_blank\">Link</a>';
+
+//  put string inside single quote for using double quote in string
+
+
+const testStr = 'I am a "double quoted" string inside \'single quotes\'.';
+
+//  for single quote in string \' used at start and end
