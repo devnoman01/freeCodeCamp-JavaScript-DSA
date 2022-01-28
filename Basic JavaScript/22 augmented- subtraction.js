4a61e4c9d3f480cf42dde13cef80844e75e0daaa
devnoman01 <devnoman01@gmail.com>
2022-01-29 02:36:35 +0600

    augmented subtraction

---
+++ Basic JavaScript/22 augmented- subtraction.js
@@ -0,0 +1,11 @@
+//  augmented subtraction
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;         // here 6 will be subtracted from the value of a
+b -= 15;        // here 15 will be subtracted from the value of b
+c -= 1;         // here 1 will be subtracted from the value of c
+
+//  '-='  assignment operator is used for augmented subtraction
