af0b235c3bf5da0ace74435494739f209e14a710
devnoman01 <devnoman01@gmail.com>
2022-01-29 04:08:45 +0600

    combining string with concatenation operator

---
+++ Basic JavaScript/28 string-concatenation-operator.js
@@ -0,0 +1,8 @@
+//  combining string with concatenation operator
+
+const ourStr = "I come first. " + "I come second.";
+
+const myStr = "This is the start. " + "This is the end.";
+
+//  use '+' concatenation operator between strings.
+//  each string should be inside "" quotation
