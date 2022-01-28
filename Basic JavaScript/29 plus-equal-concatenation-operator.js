93dcb1d476de9ad23ad11ca778bc06531161fba3
devnoman01 <devnoman01@gmail.com>
2022-01-29 04:14:42 +0600

    concatenating string with plus equal operator

---
+++ Basic JavaScript/29 plus-equal-concatenation-operator.js
@@ -0,0 +1,10 @@
+//  concatenating string with plus equal operator
+
+let ourStr = "I come first. ";
+ourStr += "I come second.";
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the secong sentence.";
+
+//  use '+=' concatenation operator between var and new string
+//  each string should be inside "" quotation
