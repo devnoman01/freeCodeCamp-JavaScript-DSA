4a5538e50e3707b05674f3ece9e4db956d7a7fde
devnoman01 <devnoman01@gmail.com>
2022-01-29 03:26:23 +0600

    augmented division

---
+++ Basic JavaScript/24 augmented-division.js
@@ -0,0 +1,12 @@
+//  augmented division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+
+a /= 12;        // value of a will be divided by 12
+b /= 4;         // value of b will be divided by 4
+c /= 11;        // value of c will be divided by 11
+
+//  '/='  assignment operator is used for augmented division
