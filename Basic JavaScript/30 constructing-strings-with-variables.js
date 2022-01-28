98f35cf92c7623355114b22773bec8af3154c66f
devnoman01 <devnoman01@gmail.com>
2022-01-29 04:21:32 +0600

    strings construction with variables

--- Basic JavaScript/30 constructing-strings-with-variables.js
+++ Basic JavaScript/30 constructing-strings-with-variables.js
@@ -1,2 +1,10 @@
 //  strings construction with variables
 
+const ourName = "freeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+const myName = "Jon";
+const myStr = "My name is " + myName + "and I am well!";
+
+// use '+' concatenation operator with both side of variable
+//  strings should be inside "" quotation
