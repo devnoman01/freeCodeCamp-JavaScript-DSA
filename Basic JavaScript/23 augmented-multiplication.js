58260027752ec6542e9a0a1d6d0fbc770a977be2
devnoman01 <devnoman01@gmail.com>
2022-01-29 03:23:19 +0600

    augmented multiplication

---
+++ Basic JavaScript/23 augmented-multiplication.js
@@ -0,0 +1,11 @@
+//  augmented multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;         // value of a will be multiplied by 5 
+b *= 3;         // value of b will be multiplied by 3
+c *= 10;        // value of c will be multiplied by 10
+
+//  '*='  assignment operator is used for augmented multiplication
