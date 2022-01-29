90a5defd447280cf06b86255348b5fa8d954235c
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:35:56 +0600

    find nth character in a string

---
+++ Basic JavaScript/35 nth-character-in-string.js
@@ -0,0 +1,11 @@
+//  find nth character in a string
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+//  '[1]' refers to second position with value 'd'
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
+//  used '[2]' to find 'v' third character in the string
+
+//  use '[]' bracket notation with the varible of string
