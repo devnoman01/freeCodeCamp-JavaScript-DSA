4f15b0428aff5c18e4e96862d3bfeb825322a6f1
devnoman01 <devnoman01@gmail.com>
2022-01-29 13:04:42 +0600

    word blanks

---
+++ Basic JavaScript/38 word-blanks.js
@@ -0,0 +1,12 @@
+// word blanks
+
+const sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "The " + myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+
+//  add extra word to complete sentence that matches the context
