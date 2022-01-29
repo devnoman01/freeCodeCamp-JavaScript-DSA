1e010671871bdae3a0a7de3c5b1b3bb87a4265c5
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:21:18 +0600

    find first character of a string

---
+++ Basic JavaScript/33 first-character-in-string.js
@@ -0,0 +1,11 @@
+//  find first character of a string
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+
+//  use '[0]' with the varible of string
