715fae6c217e5b7bc53f026a192b1af45a141a9e
devnoman01 <devnoman01@gmail.com>
2022-01-29 13:26:41 +0600

    access array data with indexes

---
+++ Basic JavaScript/41 access-array-with-index.js
@@ -0,0 +1,9 @@
+//  access array data with indexes
+
+const array = [50, 60, 70];
+const data = array[1];
+//  now data = 60, as array[1] reffers to second value 60
+
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+//  myData have equal value of first value of myArray
