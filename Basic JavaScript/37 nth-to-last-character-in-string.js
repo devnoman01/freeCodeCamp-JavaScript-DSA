c07e8cca7203ff1927c66e2c90524abfe114438b
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:50:54 +0600

    find nth-to-last character in a string

---
+++ Basic JavaScript/37 nth-to-last-character-in-string.js
@@ -0,0 +1,9 @@
+//  find nth-to-last character in a string
+
+const firstName = "Augusta";
+const thirdToLastLetter = firstName[firstName.length - 3];
+//  thirdToLastLetter will have "s" in it
+
+const lastName = "Lovelace";
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+//  secondToLastLetterOfLastName will have "c" in it
