57aa653736663ceeb2995a06001383b21ce27f35
devnoman01 <devnoman01@gmail.com>
2022-01-29 13:12:15 +0600

    strore multiple value in one variable with array

---
+++ Basic JavaScript/39 javascript-array.js
@@ -0,0 +1,9 @@
+//  strore multiple value in one variable with array
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+
+const myArray = ["Age", 50];
+
+//  all values should be inside [] square bracket
+//  values should be separated with ',' comma
+//  string value sould be inside "" double quote
