6a9ece77ccf381baa163aec8981019218947b440
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:16:11 +0600

    find the length of a string

---
+++ Basic JavaScript/32 length-of-string.js
@@ -0,0 +1,10 @@
+//  find the length of a string
+
+console.log("Alan Peter".length);
+
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+//  add '.length' with the string or with variable of string
