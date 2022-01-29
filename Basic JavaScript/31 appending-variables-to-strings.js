34fc58010f22f31eb458b89f5b28685fd5f5b1fe
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:08:26 +0600

    appending variables to strings

---
+++ Basic JavaScript/31 appending-variables-to-strings.js
@@ -0,0 +1,11 @@
+//  appending variables to strings
+
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+const someAdjective = "fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//  use '+=' assignment operator for appending variables to string
