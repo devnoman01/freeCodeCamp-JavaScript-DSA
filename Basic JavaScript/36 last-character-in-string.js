f0f93efd1950f87be11e19d9d4cd44cf5475fc4d
devnoman01 <devnoman01@gmail.com>
2022-01-29 12:40:08 +0600

    find last character of a string

---
+++ Basic JavaScript/36 last-character-in-string.js
@@ -0,0 +1,9 @@
+//  find last character of a string
+
+const firstName = "Ada";
+const lastLetter = firstName[firstName.length - 1];
+
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+//  use '.length-1' with the varible of string inside '[]' with variable
