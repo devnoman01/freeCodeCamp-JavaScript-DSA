e76e5e39750086013642a10a8d7c24078294bb67
devnoman01 <devnoman01@gmail.com>
2022-01-31 04:55:39 +0600

    creating shopping list with array

--- Basic JavaScript/48 shopping-list.js
+++ Basic JavaScript/48 shopping-list.js
@@ -1,1 +1,18 @@
 //  creating shopping list with array
+
+//  each sub-arrays have strings as first element
+//  each sub-arrays have numbers as second element
+const myList = [["HDD", 2], ["SDD", 5], ["RAM", 5], ["Battery", 3], ["Keyboard", 5], ["Mouse", 5]];
+
+console.log(myList);
+
+/*  OUTPUT:
+[
+    [ 'HDD', 2 ],
+    [ 'SDD', 5 ],
+    [ 'RAM', 5 ],
+    [ 'Battery', 3 ],
+    [ 'Keyboard', 5 ],
+    [ 'Mouse', 5 ]
+]
+*/
