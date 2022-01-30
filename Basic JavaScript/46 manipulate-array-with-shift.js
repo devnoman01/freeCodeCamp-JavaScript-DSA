67c8c9a82ce9b52f6e309ccc4566526662bb11ea
devnoman01 <devnoman01@gmail.com>
2022-01-31 04:18:28 +0600

    manipulate array with shift()

--- Basic JavaScript/46 manipulate-array-with-shift.js
+++ Basic JavaScript/46 manipulate-array-with-shift.js
@@ -1,1 +1,12 @@
 //  manipulate array with shift()
+
+//  use shift() to remove first itam from an array
+const ourArray = ["Stimpson", "J", ["cat"]];
+const removedFromOurArray = ourArray.shift();
+//  now ourArray = ["J", ["cat"]];
+//  now removedFromOurArray = "Stimpson";
+
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+//  now myArray = [["dog", 3]];
+//  now removedFromMyArray = ["John", 23];
