1e9ed65f71629d26ad6bd235d674b95fbb178c69
devnoman01 <devnoman01@gmail.com>
2022-01-31 04:26:20 +0600

    manipulate array with unshift()

--- Basic JavaScript/47 manipulate-array-with-unshift.js
+++ Basic JavaScript/47 manipulate-array-with-unshift.js
@@ -1,1 +1,11 @@
 //  manipulate array with unshift()
+
+//  use shift() to remove first item from an array
+//  use unshift() to insert item into an array at first position
+const ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift();                           //  now ourArray = ["J", "cat"];
+ourArray.unshift("Happy");                  //  now ourArray = ["Happy", "J", "cat"];
+
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();                            // now myArray = [["dog", 3]];
+myArray.unshift(["Paul", 35]);              // now myArray = [["Paul", 35], ["dog", 3]];
