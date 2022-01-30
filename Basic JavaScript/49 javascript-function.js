3be74811f796530949d04d8fdd2d06bb875a94a4
devnoman01 <devnoman01@gmail.com>
2022-01-31 05:13:33 +0600

    reusing code with function

---
+++ Basic JavaScript/49 javascript-function.js
@@ -0,0 +1,17 @@
+//  reusing code with function
+
+//  function is constructed with funtion keyword and () with function-name
+//  instructions/statements will be inside {} curly brackets
+function functionName() {
+    console.log("Hello World");
+}
+functionName();
+//  as we call the function, it will initialize the statement inside it
+
+
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
+//  after calling reusableFunction(), we will get output: Hi World.
+//  we don't need to print "Hi World" again and again, just call the function
