3d969d9aca78dee067c1917bd2a94565fa0045a2
devnoman01 <devnoman01@gmail.com>
2022-01-31 01:40:22 +0600

    manipulate array with pop()

--- Basic JavaScript/45 manipulate-array-with-pop.js
+++ Basic JavaScript/45 manipulate-array-with-pop.js
@@ -1,1 +1,12 @@
 //  manipulate array with pop()
+
+//  use pop() to remove last item from array
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);               //  output of oneDown: 6
+console.log(threeArr);              //  output of threeArr: [1, 4]
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.pop();
+//  now myArray = [["John", 23]];
