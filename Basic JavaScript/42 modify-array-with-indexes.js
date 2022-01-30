fa080c93d4e98a89c9e662fefcb461d9c42cb08a
devnoman01 <devnoman01@gmail.com>
2022-01-30 22:18:33 +0600

    modify array data with indexes

---
+++ Basic JavaScript/42 modify-array-with-indexes.js
@@ -0,0 +1,9 @@
+//  modify array data with indexes
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+//  ourArray[0] measn first element will have value 15 not 50
+
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+//  now myArray = [45, 64, 99];
