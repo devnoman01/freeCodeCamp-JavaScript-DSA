6caf97a330cc8456afb7bb5665bb9c4f12f1441f
devnoman01 <devnoman01@gmail.com>
2022-01-31 00:48:41 +0600

    manipulate array with push()

--- Basic JavaScript/44 manipulate-array-with-push.js
+++ Basic JavaScript/44 manipulate-array-with-push.js
@@ -1,1 +1,15 @@
 //  manipulate array with push()
+
+//  use push() to add element into array
+ 
+const arr1 = [1, 2, 3];
+arr1.push(4);
+//  after push now arr1 = [1, 2, 3, 4]
+
+const arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+//  after push arr2 = ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+//  after push
