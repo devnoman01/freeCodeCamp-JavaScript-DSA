8b1d1f784bf5a1bddf615df209d5f42e789ee56d
devnoman01 <devnoman01@gmail.com>
2022-01-31 05:34:17 +0600

    passing values to functions with arguments

---
+++ Basic JavaScript/50 pass-value-with-function-arguments.js
@@ -0,0 +1,12 @@
+//  passing values to functions with arguments
+
+//  parameters are used when functions are defined
+//  here sayHello function is defined with two parameters
+
+function sayHello(a, b){        // here a, b are function parameters that will receive passed value
+    console.log(a + b);         // output: Hello World
+}
+
+const x = "Hello ";
+const y = "World";
+sayHello(x, y);                 // here x, y are arguments whose value will be passed into function
