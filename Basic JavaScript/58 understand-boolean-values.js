fa677f269d93183aa7f7cf3a02ebd539eaf5d333
devnoman01 <devnoman01@gmail.com>
2022-02-06 23:31:47 +0600

    understanding boolean value as return

---
+++ Basic JavaScript/58 understand-boolean-values.js
@@ -0,0 +1,9 @@
+//  understanding boolean value as return
+
+function welcomeToBooleans() {
+    return true;    // function returns true 
+}
+
+function welcomeToBooleans() {
+    return false;    // function returns false
+}
