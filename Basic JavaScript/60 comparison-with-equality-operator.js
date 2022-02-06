c8b6b187b528fb2699c352d6ed9684378318cc9d
devnoman01 <devnoman01@gmail.com>
2022-02-06 23:59:52 +0600

    comparison with equality operator

---
+++ Basic JavaScript/60 comparison-with-equality-operator.js
@@ -0,0 +1,19 @@
+//  comparison with equality operator
+
+function equalityTest(myVal) {
+    if (myVal == 10) {
+      return "Equal";
+    }
+    return "Not Equal";
+}
+equalityTest(10);
+
+
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
