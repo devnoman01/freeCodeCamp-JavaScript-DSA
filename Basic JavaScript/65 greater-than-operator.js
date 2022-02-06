305e1f1727a428889e2ded19232d7d6c03feebea
devnoman01 <devnoman01@gmail.com>
2022-02-07 02:40:29 +0600

    comparison with greater than operator

---
+++ Basic JavaScript/65 greater-than-operator.js
@@ -0,0 +1,24 @@
+//  comparison with greater than operator
+
+/*
+5   >  3    true
+7   > '3'   true
+2   >  3    false
+'1' >  9    false
+*/
+
+function testGreaterThan(val) {
+    if (val > 100) {
+      return "Over 100";
+    }
+  
+    if (val > 10) {
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+testGreaterThan(10);    // return: "10 or Under"
+testGreaterThan(19);    // return: "10 or Over"
+testGreaterThan(150);   // return: "Over 100"
