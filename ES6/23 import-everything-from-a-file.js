97e6ee1410351a01206a4c4ef06ca408d2b76d54
devnoman01 <devnoman01@gmail.com>
2022-03-15 02:05:00 +0600

    use * to import everything from a file

---
+++ ES6/23 import-everything-from-a-file.js
@@ -0,0 +1,9 @@
+// use * to import everything from a file
+
+import * as importFunctions from './21 use-export-to-share-code-block.js';
+
+importFunctions.add(4, 7);
+importFunctions.sub(7, 2);
+
+importFunctions.uppercaseString("hello");
+importFunctions.lowercaseString("WORLD!");
