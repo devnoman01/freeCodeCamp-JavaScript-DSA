8f3ce0cc6c79bd4493b9033561703c9af361f8f8
devnoman01 <devnoman01@gmail.com>
2022-03-15 01:55:32 +0600

    reuse javascript code using import

---
+++ ES6/22 reuse-javascript-code-using-import.js
@@ -0,0 +1,11 @@
+// reuse javascript code using import
+
+import { add } from './21 use-export-to-share-code-block.js';
+
+const sum = add(5, 3);
+
+
+import { uppercaseString, lowercaseString } from './21 use-export-to-share-code-block.js';
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
